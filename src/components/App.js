67144cf0747d15ec346f2cbee24d807cc950327e
JBeni <jitca.beniamin@gmail.com>
2021-02-12 14:37:54 +0200

    creating selectors

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,9 @@ import {
   loadToken,
   loadExchange
 } from '../store/interactions';
+import { contractsLoadedSelector } from '../store/selectors';
+import Navbar from './Navbar';
+import Content from './Content';
 
 class App extends React.Component {
 
@@ -16,98 +19,26 @@ class App extends React.Component {
 
   async loadBlockchainData(dispatch) {
     const web3 = await loadWeb3(dispatch);
+    await web3.eth.net.getNetworkType();
     const networkId = await web3.eth.net.getId();
-    const accounts = await loadAccount(web3, dispatch);
+    await loadAccount(web3, dispatch);
     const token = await loadToken(web3, networkId, dispatch);
+    if (!token) {
+      window.alert('Token smart contract not detected on the current network. Please select another network with MetaMask.');
+      return;
+    }
     const exchange = await loadExchange(web3, networkId, dispatch);
+    if (!exchange) {
+      window.alert('Exchange smart contract not detected on the current network. Please select another network with MetaMask.');
+      return;
+    }
   }
 
   render () {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 1</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 2</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 3</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="content">
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Navbar />
+        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
       </div>
     );
   }
@@ -115,8 +46,8 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    // TODO: Fill me in...
-  }
+    contractsLoaded: contractsLoadedSelector(state)
+  };
 }
 
 export default connect(mapStateToProps)(App);
