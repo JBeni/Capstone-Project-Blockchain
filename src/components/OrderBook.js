3b6d640e362936817b257a91160c1cde04785f50
JBeni <jitca.beniamin@gmail.com>
2021-02-13 15:14:06 +0200

    implementing Fill Orders card

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Spinner from './Spinner';
-import { orderBookSelector, orderBookLoadedSelector } from '../store/selectors';
+import { fillOrder } from '../store/interactions';
+import {
+    orderBookSelector,
+    orderBookLoadedSelector,
+    exchangeSelector,
+    accountSelector,
+    orderFillingSelector
+} from '../store/selectors';
 
-const renderOrder = (order) =>{
+const renderOrder = (order, props) => {
+    const { dispatch, exchange, account } = props;
+
     return(
-        <tr key={order.id}>
-            <td>{order.tokenAmount}</td>
-            <td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
-            <td>{order.etherAmount}</td>
-        </tr>
+        <OverlayTrigger
+            key={order.id}
+            placement='auto'
+            overlay={
+                <Tooltip id={order.id}>
+                {`Click here to ${order.orderFillAction}`}
+                </Tooltip>
+            }
+        >
+            <tr
+                key={order.id}
+                className="order-book-order"
+                onClick={(e) => fillOrder(dispatch, exchange, order, account)}
+            >
+                <td>{order.tokenAmount}</td>
+                <td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+                <td>{order.etherAmount}</td>
+            </tr>
+        </OverlayTrigger>
     );
 }
 
@@ -49,9 +73,14 @@ class OrderBook extends React.Component {
 }
 
 function mapStateToProps(state) {
+    const orderBookLoaded = orderBookLoadedSelector(state);
+    const orderFilling = orderFillingSelector(state);
+
     return {
         orderBook: orderBookSelector(state),
-        showOrderBook: orderBookLoadedSelector(state)
+        showOrderBook: orderBookLoaded && !orderFilling,
+        exchange: exchangeSelector(state),
+        account: accountSelector(state)
     };
 }
 
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -71,7 +71,7 @@ const decorateOrder = (order) => {
     let etherAmount;
     let tokenAmount;
 
-    if (order.tokenGive == ETHER_ADDRESS) {
+    if (order.tokenGive === ETHER_ADDRESS) {
         etherAmount = order.amountGive;
         tokenAmount = order.amountGet;
     } else {
@@ -173,7 +173,7 @@ const decorateOrderBookOrder = (order) => {
         ...order,
         orderType,
         orderTypeClass: (orderType === 'buy' ? GREEN : RED),
-        orderFillClass: (orderType === 'buy' ? 'sell' : 'buy')
+        orderFillAction: (orderType === 'buy' ? 'sell' : 'buy')
     });
 }
 
@@ -309,11 +309,5 @@ const buildGraphData = (orders) => {
 const orderCancelling = state => get(state, 'exchange.orderCancelling', false);
 export const orderCancellingSelector = createSelector(orderCancelling, status => status);
 
-
-
-
-
-
-
-
-
+const orderFilling = state => get(state, 'exchange.orderFilling', false)
+export const orderFillingSelector = createSelector(orderFilling, status => status)
